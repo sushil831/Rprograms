4cd64bb155c5ad54997d5337051c46282b35a45e
sushil831 <73579161+sushil831@users.noreply.github.com>
2023-08-30 17:50:23 +0530

    Add files via upload

---
+++ rintro.R
@@ -0,0 +1,78 @@
+"Hello world"
+#Hello world
+5+5
+name1 = 'sUSHil'
+print(name1)
+age = 25
+num ="20"
+age = age+5
+age
+
+text = "Excellent"
+paste("excelr is",text)
+paste("studentys are",text)
+myvar = "rahul"
+my_var = "saini"
+myVar = "manish"
+MYVAR ="ANKIT"
+myvar2 = "bunty"
+this.year =2023
+x = 10.5
+class(x)
+class(name1)
+
+y = 1000L
+class(y)
+x = "R is exciting"
+class(x)
+x = TRUE
+class(x)
+3>5
+3<4
+4==5
+1!=3
+2!=3
+x= 10
+y = 20
+x+y
+x-y
+x*y
+x/y
+x^2
+x %% y
+x%/%y
+2*pi
+??constants
+pi^2
+letters
+month.name
+x=50
+50 -> x
+max(35,67,57)
+min(36,34,23)
+sqrt(25)
+abs(-14)
+fruits = c("Banana","Apple","Orange","Strawbery")
+class(fruits)
+v1 = c(2,4,5,6,7)
+class(v1)
+max(v1)
+min(v1)
+v2 = c(T,F,TRUE,FALSE)
+class(v2)
+v2
+mix = c(T,F)
+class(mix)
+x = c(2,5,8,4)
+y =c(1,9,9,9)
+x+y
+x*5
+(x+y)*5
+#sequence
+1:10
+1:1000
+49:67
+seq(1,50,5)
+seq(1,50,by=3)
+seq(1,10)
+seq(10,1,-2)
